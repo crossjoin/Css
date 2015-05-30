59f45f1ded97b011c465e70344d2a122244dd3a6
Christoph Ziegenberg <christoph@ziegenberg.com>
2015-05-30 16:36:51 +0200

    Optimized error message
    Optimized error message to better detect parser errors.

--- src/Crossjoin/Css/Format/Rule/AtRuleConditionalAbstract.php
+++ src/Crossjoin/Css/Format/Rule/AtRuleConditionalAbstract.php
@@ -20,9 +20,12 @@ implements HasRulesInterface, RuleGroupableInterface
         if ($rule instanceof RuleGroupableInterface) {
             $this->rules[] = $rule;
         } else {
+            $parentClassName = get_class($this);
+            $childClassName = get_class($rule);
             throw new \InvalidArgumentException(
                 "Invalid rule instance. Instance of 'RuleGroupableInterface' expected, " .
-                "because only nested statements can be added to conditional group rules"
+                "because only nested statements can be added to conditional group rules. " .
+                "Tried to add rule of type '$childClassName' to '$parentClassName'."
             );
         }
 
